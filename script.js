33c518c687e1cd41d124ee30bf4a643f3ffcf20b
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-01-22 12:57:03 +0530

    fix:search option

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 let content = "";
 
-// console.log("hi");
-
 let recipes_list = JSON.parse(localStorage.getItem("recipes_list")) || [];
-// recipes_list.map((val) => {
-//   content =
-//     content +
-//     `
-//     <tr>
-//               <td class="border-2 text-center">${val.id}</td>
-//               <td class="border-2 text-center">${val.name}</td>
-//               <td class="border-2 text-center">${val.type}</td>
-//               <td class="border-2 text-center">${val.description}</td>
-//               <td class="border-2 text-center">
-//                 <div class="flex gap-8 justify-around my-4">
-//                     <button class="bg-amber-400 px-8 rounded-2xl" onclick="EditRecipe('${val.id}','${val.name}','${val.type}','${val.description}')">Edit</button>
-//                     <button class="bg-red-600 px-8 rounded-2xl" onclick="DeleteRecipe('${val.id}')">Delete</button></td>
-//                 </div>
-//             </tr>
-//             `;
-// });
+
 
 recipes_list.map((val) => {
   content =
